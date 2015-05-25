8fde9024d922776a90d72141c0374c7265eedbb3
Hack Reactor Students <accounts@hackreactor.com>
2015-05-25 15:45:11 -0700

    Fetch message with GET, create chatrooms behavior

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -16,3 +16,27 @@ app.send = function(message){
     }
   });
 };
+app.fetch = function(url) {
+  $.ajax({
+    // always use this url
+    url: url,
+    type: 'GET',
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message recieved');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message');
+    }
+  });
+};
+app.clearMessages = function(){
+  $('#chats').children().remove();
+};
+app.addMessage = function(message) {
+  $('#chats').append('<div>' + message.username + ": " + message.text + '</div>');
+};
+app.addRoom = function(roomName){
+  $('#roomSelect').append('<div></div>');
+};
